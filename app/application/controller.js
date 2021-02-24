ec894960510fd2ad3879a7c99fd2b236d7cda5ae
Jimmy <ultish@gmail.com>
2021-02-24 21:21:45 +1100

    init

---
+++ app/application/controller.js
@@ -0,0 +1,12 @@
+import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+
+export default Controller.extend({
+  mdeValue: '',
+  simpleMdeOptions: computed(function() {
+    return {
+
+    }
+  })
+
+});
